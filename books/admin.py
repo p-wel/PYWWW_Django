cdd5cbedeccda27a05dbcaa26089c58ccc2c98ba
p-wel <wojtowiczp97@gmail.com>
2021-12-05 22:07:58 +0100

    Module1_done

--- books/admin.py
+++ books/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from books.models import Book
+from .models import Book
 
 admin.site.register(Book)
--- books/views.py
+++ books/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def library(request):
+    return HttpResponse("Tu będzie moja biblioteka")
